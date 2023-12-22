28eeb03f95669c8167b9856b321f22695a8ba884
SerkanCuteli <serkancuteli@gmail.com>
2023-12-22 19:08:51 +0100

    Added: DB Automation Sample Uploaded

--- src/test/java/com/eurotech/runners/CukesRunner.java
+++ src/test/java/com/eurotech/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/eurotech/stepDefinitions",
         dryRun = false,
-        tags = "@db"
+        tags = "@test"
 
 )
 public class CukesRunner {
